020b888471e0019bdf5bcdb189e62c6b5a743b4e
pkderlam87 <62060342+pkderlam87@users.noreply.github.com>
2022-09-19 12:02:42 +0200

    Will add function to send e-mail

--- api/common-contact-form/controllers/common-contact-form.js
+++ api/common-contact-form/controllers/common-contact-form.js
@@ -5,4 +5,17 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async customContact(ctx) {
+        await strapi.plugins['email'].services.email.send({
+            to: 'valid email address',
+            from: 'your verified email address', //e.g. single sender verification in SendGrid
+            cc: 'valid email address',
+            bcc: 'valid email address',
+            replyTo: 'valid email address',
+            subject: 'The Strapi Email plugin worked successfully',
+            text: 'Hello world!',
+            html: 'Hello world!',
+        });
+    }
+};
